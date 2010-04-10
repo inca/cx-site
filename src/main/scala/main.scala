6142c83cebc6f9d0e6d27103dbf83fc786eb4aa9
incarnate <incarnate@whiteants.net>
2010-04-10 10:06:14 +0400

    Added circumflex layout and styles.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,10 +1,14 @@
 package ru.circumflex.site
 
-import _root_.ru.circumflex.core.RequestRouter
 import _root_.ru.circumflex.freemarker.FreemarkerHelper
 import _root_.ru.ciridiri.CiriDiri
+import java.text.SimpleDateFormat
+import java.util.Date
+import ru.circumflex.core.{Circumflex, RequestRouter}
 
 class MainRouter extends RequestRouter
     with FreemarkerHelper {
+  ctx += "host" -> header("Host")
+  ctx += "currentYear" -> new SimpleDateFormat("yyyy").format(new Date)
   new CiriDiri()
 }
